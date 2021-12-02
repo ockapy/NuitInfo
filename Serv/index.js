b4563b8d51c02798b4c0064504bd79060e0cf3fb
unknown <anas38.gt@gmail.com>
2021-12-02 22:47:50 +0100

    Création des Dossiers + DB.ini + connectBdd + index.js

---
+++ Serv/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+
+app.use(express.static('views'))
+app.use(express.static('public'))
+
+
+
+
+
+app.listen(port, () => {
+    console.log('Ecoute du port ' + port);
+});
+
+
+
+
+
+
+
+
+
+// inclure les dépendances et middlewares 
+const express = require('express') 
+const ejs = require('ejs')
+const mysql = require('mysql')
+const iniparser = require('iniparser')
+
+const Routeur = require('./routes/pharmaRoute')
+
+// adresse ou port utilisé
+let adresse = 3000; // port : 3000 //
+
+// activation des dépendances 
+let app = express()
+app.set('view engine', 'ejs')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Ecoute //
+app.listen(adresse, () => console.log('le serveur est prêt.'))
+
+app.use('/PharmaScieSauteuse', Routeur);
+
+// erreur 404 //
+app.use((req, res) => {
+    res.status(404).render('erreur')
+});
---
+++ Serv/models/bddSauvetage.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql')
+const iniparser = require('iniparser')
+
+// récupération des paramètres et préparation de connexion à la BDD
+let configDB = iniparser.parseSync('./DB.ini')
+let db = mysql.createConnection({
+    host:configDB['Sauvetage']['host'],
+    user:configDB['Sauvetage']['user'],
+    password:configDB['Sauvetage']['password'],
+    database:configDB['Sauvetage']['database']
+})
+
+// connexion à la base de données
+db.connect((err) => {
+    if (!err) console.log('BDD connectée.')
+    else console.log('BDD connexion échouée \n Erreur: '+JSON.stringify(err))
+})
+
+
+module.exports = dbSauvetage
